aaf0a24fd7d4a4ac0865df5d553fb26014956b86
Fellipe Marcel <fmrclms@gmail.com>
2023-04-14 09:03:28 -0300

    feat: implemented search form

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchInputContent: '',
+    isButtonDisabled: true,
+  };
+
+  searchInputValidation = () => {
+    const { searchInputContent } = this.state;
+    const MIN_LENGTH = 2;
+    const validInput = searchInputContent.length >= MIN_LENGTH;
+    this.setState({
+      isButtonDisabled: !validInput,
+    });
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.searchInputValidation);
+  };
+
   render() {
+    const { searchInputContent, isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="Search__Input">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="searchInputContent"
+              id="Search__Input"
+              value={ searchInputContent }
+              onChange={ this.onInputChange }
+            />
+          </label>
+        </form>
+        <button
+          data-testid="search-artist-button"
+          disabled={ isButtonDisabled }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
