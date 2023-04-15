e0265138d19a89976d5e4bfae74e6f5e5bfd4f71
Fellipe Marcel <fmrclms@gmail.com>
2023-04-15 11:52:12 -0300

    feat: implemented mecanism to add song to favorite songs

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  state = {
+    checkedMap: {},
+    isLoading: false,
+  };
+
+  handleCheckbox = async ({ target: { id, checked } }) => {
+    this.setState({
+      isLoading: true,
+    });
+
+    this.setState((prevState) => ({
+      checkedMap: {
+        ...prevState.checkedMap,
+        [id]: checked,
+      },
+    }));
+
+    if (checked) {
+      await addSong(id);
+    }
+
+    this.setState({
+      isLoading: false,
+    });
+  };
+
   render() {
     const { songs } = this.props;
+    const { checkedMap, isLoading } = this.state;
+
+    if (isLoading) {
+      return <Loading />;
+    }
+
     return (
-      songs.map(({ previewUrl, trackName }, index) => (
+      songs.map(({ previewUrl, trackName, trackId }, index) => (
         <div key={ index }>
           <p>{trackName}</p>
           <audio data-testid="audio-component" src={ previewUrl } controls>
@@ -13,6 +47,16 @@ class MusicCard extends Component {
             O seu navegador não suporta o elemento
             <code>audio</code>
           </audio>
+          <label data-testid={ `checkbox-music-${trackId}` } htmlFor={ trackId }>
+            Favorita
+            <input
+              type="checkbox"
+              name={ `checkbox-music-${trackId}` }
+              id={ trackId }
+              checked={ checkedMap[trackId] || false } // Usa o estado guardado para o ID, ou false se não existir
+              onChange={ this.handleCheckbox }
+            />
+          </label>
         </div>
       ))
     );
